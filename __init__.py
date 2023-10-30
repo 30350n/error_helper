bd557b8cfa9646704f58d2bb3349100d9e838451
Bobbe <maxschlecht@web.de>
2023-10-30 18:16:11 +0100

    Handle dynamically adding newlines when calling error

--- __init__.py
+++ __init__.py
@@ -8,25 +8,32 @@ COLOR_ERROR = "\033[91m"
 COLOR_END = "\033[0m"
 
 INFO_END = "\n"
+_LAST_END = [INFO_END]
 CLEAR_LINE = "\033[2K"
 
 def info(*values, sep=" ", end=None, file=_stdout, flush=False):
     output = f"{CLEAR_LINE}{COLOR_INFO}{sep.join(map(str, values))}{COLOR_END}"
     end = INFO_END if end is None else end
     print(output, end=end, flush=flush, file=file)
+    _LAST_END[0] = end
 
 def hint(*values, sep=" ", end="\n", file=_stdout, flush=False):
     output = f"{CLEAR_LINE}{COLOR_HINT}({sep.join(map(str, values))}){COLOR_END}"
     print(output, end=end, flush=flush, file=file)
+    _LAST_END[0] = end
 
 def success(*values, sep=" ", end="\n", file=_stdout, flush=False, prefix="successfully "):
     output = f"{CLEAR_LINE}{COLOR_SUCCESS}{prefix}{sep.join(map(str, values))}{COLOR_END}"
     print(output, end=end, flush=flush, file=file)
+    _LAST_END[0] = end
 
 def warning(*values, sep=" ", end="\n", file=_stdout, flush=False, prefix="warning: "):
     output = f"{CLEAR_LINE}{COLOR_WARNING}{prefix}{sep.join(map(str, values))}{COLOR_END}"
     print(output, end=end, flush=flush, file=file)
+    _LAST_END[0] = end
 
 def error(*values, sep=" ", end="\n", file=_stderr, flush=False, prefix="error: "):
-    output = f"{CLEAR_LINE}{COLOR_ERROR}{prefix}{sep.join(map(str, values))}{COLOR_END}"
+    PREFIX = "" if _LAST_END[0] and _LAST_END[0][-1] == "\n" else "\n"
+    output = f"{PREFIX}{COLOR_ERROR}{prefix}{sep.join(map(str, values))}{COLOR_END}"
     print(output, end=end, flush=flush, file=file)
+    _LAST_END[0] = end
