c7e2694c34867c6ae0d205b34edac7d82adfbf18
Max Schlecht <maxschlecht@web.de>
2025-03-31 19:07:22 +0200

    Refactor _line_end global variable, add type hints for all arguments

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from sys import stderr as _stderr
+from typing import TextIO
 
 COLOR_INFO = "\033[94m"
 COLOR_HINT = "\033[2;3m"
@@ -14,42 +15,96 @@ BOLD_END = "\033[22m"
 ITALIC = "\033[3m"
 ITALIC_END = "\033[23m"
 
-INFO_END = "\n"
-_LAST_END = [INFO_END]
 CLEAR_LINE = "\033[2K"
 
-def info(*values, sep=" ", end=None, file=None, flush=False):
+INFO_END = "\n"
+_last_end = INFO_END
+
+
+def info(
+    *values: object,
+    sep: str = " ",
+    end: str | None = None,
+    file: TextIO | None = None,
+    flush: bool = False,
+):
+    global _last_end
     output = f"{CLEAR_LINE}{COLOR_INFO}{sep.join(map(str, values))}{COLOR_END}"
     end = INFO_END if end is None else end
     print(output, end=end, flush=flush, file=file)
-    _LAST_END[0] = end
+    _last_end = end
 
-def hint(*values, sep=" ", end="\n", file=None, flush=False):
+
+def hint(
+    *values: object,
+    sep: str = " ",
+    end: str | None = "\n",
+    file: TextIO | None = None,
+    flush: bool = False,
+):
+    global _last_end
     output = f"{CLEAR_LINE}{COLOR_HINT}({sep.join(map(str, values))}){COLOR_END}"
     print(output, end=end, flush=flush, file=file)
-    _LAST_END[0] = end
+    _last_end = end
 
-def prompt(*values, sep=" ", end=":\n", file=None, flush=False, prefix="\n"):
+
+def prompt(
+    *values: object,
+    sep: str = " ",
+    end: str | None = ":\n",
+    file: TextIO | None = None,
+    flush: bool = False,
+    prefix: str = "\n",
+):
+    global _last_end
     output = f"{CLEAR_LINE}{COLOR_PROMPT}{prefix}{sep.join(map(str, values))}{COLOR_END}"
     print(output, end=end, flush=flush, file=file)
-    _LAST_END[0] = end
+    _last_end = end
 
-def prompt_input(*values, end=": ", strip=True):
+
+def prompt_input(*values: object, end: str | None = ": ", strip: bool = True):
     prompt(*values, end=end, prefix="")
     return input().strip() if strip else input()
 
-def success(*values, sep=" ", end="\n", file=None, flush=False, prefix="successfully "):
+
+def success(
+    *values: object,
+    sep: str = " ",
+    end: str | None = "\n",
+    file: TextIO | None = None,
+    flush: bool = False,
+    prefix: str = "successfully ",
+):
+    global _last_end
     output = f"{CLEAR_LINE}{COLOR_SUCCESS}{prefix}{sep.join(map(str, values))}{COLOR_END}"
     print(output, end=end, flush=flush, file=file)
-    _LAST_END[0] = end
+    _last_end = end
 
-def warning(*values, sep=" ", end="\n", file=None, flush=False, prefix="warning: "):
+
+def warning(
+    *values: object,
+    sep: str = " ",
+    end: str | None = "\n",
+    file: TextIO | None = None,
+    flush: bool = False,
+    prefix: str = "warning: ",
+):
+    global _last_end
     output = f"{CLEAR_LINE}{COLOR_WARNING}{prefix}{sep.join(map(str, values))}{COLOR_END}"
     print(output, end=end, flush=flush, file=file)
-    _LAST_END[0] = end
+    _last_end = end
 
-def error(*values, sep=" ", end="\n", file=_stderr, flush=False, prefix="error: "):
-    PREFIX = "" if _LAST_END[0] and _LAST_END[0][-1] == "\n" else "\n"
-    output = f"{PREFIX}{COLOR_ERROR}{prefix}{sep.join(map(str, values))}{COLOR_END}"
+
+def error(
+    *values: object,
+    sep: str = " ",
+    end: str | None = "\n",
+    file: TextIO | None = _stderr,
+    flush: bool = False,
+    prefix: str = "error: ",
+):
+    global _last_end
+    extra_prefix = "" if _last_end and _last_end[-1] == "\n" else "\n"
+    output = f"{extra_prefix}{COLOR_ERROR}{prefix}{sep.join(map(str, values))}{COLOR_END}"
     print(output, end=end, flush=flush, file=file)
-    _LAST_END[0] = end
+    _last_end = end
